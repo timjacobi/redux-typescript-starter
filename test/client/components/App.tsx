0587e25074bcd22a7225e4069ac21705b00241d6
Tim Jacobi <dev@tim-jacobi.com>
2016-12-03 02:09:25 +0000

    update tests

--- test/client/components/App.tsx
+++ test/client/components/App.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import * as mocha from 'mocha';
+import * as Immutable from 'seamless-immutable';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import { spy } from 'sinon';
+
 import { App } from '../../../client/components/App';
+import { noop } from '../../../client/util';
 
 describe('<App />', () => {
   it('should render a button', () => {
-    const wrapper = shallow(<App requestUsers={() => {}} users={[]} />);
+    const wrapper = shallow(<App requestUsers={noop} users={Immutable.from([])} />);
     expect(wrapper.find('button')).to.have.length(1);
+  });
+
+  it('should call the requestUsers property when the button is clicked', () => {
+    const requestUsers = spy();
+    const wrapper = shallow(<App requestUsers={requestUsers} users={Immutable.from([])} />);
+    wrapper.find('button').simulate('click');
+    expect(requestUsers.called).to.be.true;
   });
 });
