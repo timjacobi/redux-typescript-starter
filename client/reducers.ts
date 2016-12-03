97c307c248b4fb13093009cca98046643d30d2f1
Tim Jacobi <dev@tim-jacobi.com>
2016-12-03 01:12:16 +0000

    introduce User type

--- client/reducers.ts
+++ client/reducers.ts
@@ -2,18 +2,21 @@ import * as Immutable from 'seamless-immutable';
 import { ImmutableArray } from 'seamless-immutable';
 import { combineReducers } from 'redux';
 
-import { 
-  Action, 
-  ActionTypes 
+import {
+  Action,
+  ActionTypes
 } from './actions';
 
-function users(users: ImmutableArray<any> = Immutable.from([]), action: Action): ImmutableArray<any> {
-  switch(action.type){
+import {
+  User
+} from './entities';
+
+function users(users: ImmutableArray<User> = Immutable.from([]), action: Action): ImmutableArray<any> {
+  switch (action.type) {
     case ActionTypes.SET_USERS:
-    return Immutable.from(action.payload);
-    default:
-    return users;
+      return Immutable.from(action.payload);
   }
+  return users;
 };
 
 export default combineReducers({
